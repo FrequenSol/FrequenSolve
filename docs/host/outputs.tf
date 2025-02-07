ce4e15048a49c4a3962148c695beb53604b85681
Sam Badger <smalbadger@gmail.com>
2025-02-06 22:32:17 -0700

    add cloudfront distribution id to terraform outputs

--- docs/host/outputs.tf
+++ docs/host/outputs.tf
@@ -3,6 +3,11 @@ output "cloudfront_domain" {
   value       = aws_cloudfront_distribution.docs.domain_name
 }
 
+output "cloudfront_distribution_id" {
+  description = "CloudFront distribution ID"
+  value       = aws_cloudfront_distribution.docs.id
+}
+
 output "website_endpoint" {
   description = "S3 static website hosting endpoint"
   value       = aws_s3_bucket_website_configuration.docs.website_endpoint
