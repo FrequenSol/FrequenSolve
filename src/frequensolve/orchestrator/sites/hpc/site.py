7956bf53dc59ca27c00b585c4cb48bb8ad2ba3f5
Sam Badger <smalbadger@gmail.com>
2026-08-25 23:49:13 -0600

    Keep Slurm validation type-safe

--- src/frequensolve/orchestrator/sites/hpc/site.py
+++ src/frequensolve/orchestrator/sites/hpc/site.py
@@ -23,7 +23,7 @@ from datetime import datetime, timezone
 from importlib.resources import as_file, files
 from pathlib import Path
 from select import select
-from typing import Any, Dict, List, Literal, Mapping, Optional, Type, Union
+from typing import Any, Dict, List, Literal, Mapping, Optional, Type, Union, cast
 
 from frequensolve._optional import optional_dependency_error
 
@@ -2778,7 +2778,7 @@ class SlurmSite(BaseSite):
 
         # Unpack keyword arguments
         queue = _safe_slurm_directive(kwargs.pop("queue", self.config.queue), "queue")
-        config = self.config_for_partition(queue)
+        config = self.config_for_partition(cast(str, queue))
         account = _safe_slurm_directive(
             kwargs.pop("account", self.config.account), "account"
         )
@@ -2786,7 +2786,10 @@ class SlurmSite(BaseSite):
         name = _safe_slurm_directive(name, "name") or "FrequenSolve"
         duration = _safe_slurm_directive(duration, "duration") or duration
         stdout = _safe_slurm_directive(stdout, "stdout") or stdout
-        notify_on = _safe_slurm_directive(notify_on, "notify_on")
+        notify_on = cast(
+            Optional[Literal["begin", "end", "fail", "all", "none"]],
+            _safe_slurm_directive(notify_on, "notify_on"),
+        )
         notify_email = _safe_slurm_directive(notify_email, "notify_email")
         rank_values = _normalize_rank_aliases(
             {
